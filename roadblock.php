e43c86917e3b6d316bda34bc3bfa697bf4625ed0
Willem Toorop <willem@nlnetlabs.nl>
2015-07-23 18:44:58 +0200

    Incremental dial

--- roadblock.php
+++ roadblock.php
@@ -46,6 +46,7 @@ if (php_getdns_util_convert_array($extensionsArr, $extensions)) {
 }
 
 $pgrade = 0;
+$pfail = False;
 $grade = 0;
 $respDict = 0;
 if (php_getdns_general_sync($context,
@@ -64,9 +65,10 @@ if ($respArr["replies_tree"][0]["header"]["ancount"] > 0) {
 }
 if ($respArr["replies_tree"][0]["dnssec_status"] == GETDNS_DNSSEC_SECURE) {
 	$grade += 1;
-	$pgrade += 1;
+	if (!$pfail) { $pgrade += 1; }
 	print("<font color=\"green\">Query for alg-8-nsec3.dnssec-test.org had secure answer</font>: $grade<br>");
 } else {
+	$pfail = True;
 	print("<font color=\"red\">Query for alg-8-nsec3.dnssec-test.org did not have an secure answer</font>: $grade<br>");
 }
 
@@ -88,9 +90,10 @@ if ($respArr["replies_tree"][0]["header"]["ancount"] == 0) {
 }
 if ($respArr["replies_tree"][0]["dnssec_status"] == GETDNS_DNSSEC_SECURE) {
 	$grade += 1;
-	$pgrade += 1;
+	if (!$pfail) { $pgrade += 1; }
 	print("<font color=\"green\">Query for realy-doesnotexist.dnssec-test.org. was secure</font>: $grade<br>");
 } else {
+	$pfail = True;
 	print("<font color=\"red\">Query for realy-doesnotexist.dnssec-test.org. was not secure</font>: $grade<br>");
 }
 php_getdns_dict_destroy($respDict);
@@ -113,9 +116,10 @@ if ($respArr["replies_tree"][0]["header"]["ancount"] == 0) {
 if ($respArr["replies_tree"][0]["header"]["rcode"] == GETDNS_RCODE_SERVFAIL
     && $respArr["replies_tree"][0]["header"]["ad"] == 0) {
 	$grade += 1;
-	$pgrade += 1;
+	if (!$pfail) { $pgrade += 1; }
 	print("<font color=\"green\">rcode for dnssec-failed.org was SERVFAIL</font>: $grade<br>");
 } else {
+	$pfail = True;
 	print("<font color=\"red\">rcode for dnssec-failed.org was not SERVFAIL</font>: $grade<br>");
 }
 
@@ -158,18 +162,11 @@ error:
 <br>
 Also try:
 <table>
-<tr><td>Dyn Internet Guide</td><td><a href="?addr=216.146.35.35">216.146.35.35</a></td><td><a href="?addr=216.146.36.36">216.146.36.36</a></td></tr>
-<tr><td>Level 3</td><td><a href="?addr=209.244.0.3">209.244.0.3</a></td><td><a href="?addr=209.244.0.4">209.244.0.4</a></td></tr>
-<tr><td>Google</td><td><a href="?addr=8.8.8.8">8.8.8.8</a></td><td><a href="?addr=8.8.4.4">8.8.4.4</a></td></tr>
-<tr><td>Comodo Secure DNS</td><td><a href="?addr=8.26.56.26">8.26.56.26</a></td><td><a href="?addr=8.20.247.20">8.20.247.20</a></td></tr>
-<tr><td>OpenDNS Home</td><td><a href="?addr=208.67.222.222">208.67.222.222</a></td><td><a href="?addr=208.67.220.220">208.67.220.220</a></td></tr>
 <tr><td>DNS Advantage</td><td><a href="?addr=156.154.70.1">156.154.70.1</a></td><td><a href="?addr=156.154.71.1">156.154.71.1</a></td></tr>
-<tr><td>Norton ConnectSafe</td><td><a href="?addr=198.153.192.40">198.153.192.40</a></td><td><a href="?addr=198.153.194.40">198.153.194.40</a></td></tr>
-<tr><td>ScrubIT</td><td><a href="?addr=67.138.54.120">67.138.54.120</a></td><td><a href="?addr=207.225.209.77">207.225.209.77</a></td></tr>
-<tr><td>OpenNIC</td><td><a href="?addr=74.207.247.4">74.207.247.4</a></td><td><a href="?addr=64.0.55.201">64.0.55.201</a></td></tr>
-<tr><td>Public-Root</td><td><a href="?addr=199.5.157.131">199.5.157.131</a></td><td><a href="?addr=208.71.35.137">208.71.35.137</a></td></tr>
-<tr><td>SmartViper</td><td><a href="?addr=208.76.50.50">208.76.50.50</a></td><td><a href="?addr=208.76.51.51">208.76.51.51</a></td></tr>
-<tr><td>DNSResolvers.com</td><td><a href="?addr=205.210.42.205">205.210.42.205</a></td><td><a href="?addr=64.68.200.200">64.68.200.200</a></td></tr>
+<tr><td>Dyn Internet Guide</td><td><a href="?addr=216.146.35.35">216.146.35.35</a></td><td><a href="?addr=216.146.36.36">216.146.36.36</a></td></tr>
+<tr><td>Google</td><td><a href="?addr=8.8.8.8">8.8.8.8</a></td><td><a href="?addr=8.8.4.4">8.8.4.4</a></td></tr>
+<tr><td>Level 3</td><td><a href="?addr=209.244.0.3">209.244.0.3</a></td><td><a href="?addr=209.244.0.4">209.244.0.4</a></td></tr>
+<tr><td>OpenDNS Home</td><td><a href="?addr=208.67.222.222">208.67.222.222</a></td><td><a href="?addr=208.67.220.220">208.67.220.220</a></td></tr>
 <tr><td>Verisign</td><td><a href="?addr=198.41.2.2">198.41.2.2</a></td><td><a href="?addr=198.41.1.1">198.41.1.1</a></td></tr>
 </table><dl>
 <dt>Github:</dt><dd><a href="https://github.com/getdnsapi/IETF93HackathonPHP">https://github.com/getdnsapi/IETF93HackathonPHP</a></dd>
